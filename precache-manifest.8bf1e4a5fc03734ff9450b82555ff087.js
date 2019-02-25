091513a7492ece5e7adff1dcd8d480a869618883
Michael Stromberg <mstrombe@sscinc.com>
2019-02-24 21:49:04 -0800

    Updates

---
+++ precache-manifest.8bf1e4a5fc03734ff9450b82555ff087.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5cac1f1ec0ecc64a1993",
+    "url": "/comfy-calculators/static/css/main.89d32ae1.chunk.css"
+  },
+  {
+    "revision": "5cac1f1ec0ecc64a1993",
+    "url": "/comfy-calculators/static/js/main.5cac1f1e.chunk.js"
+  },
+  {
+    "revision": "b67b32e91a92a6251685",
+    "url": "/comfy-calculators/static/js/runtime~main.b67b32e9.js"
+  },
+  {
+    "revision": "ff24f7d2ae09382df00f",
+    "url": "/comfy-calculators/static/css/2.3fa498fc.chunk.css"
+  },
+  {
+    "revision": "ff24f7d2ae09382df00f",
+    "url": "/comfy-calculators/static/js/2.ff24f7d2.chunk.js"
+  },
+  {
+    "revision": "aaac14973e6bd555a3c87b3f50576228",
+    "url": "/comfy-calculators/index.html"
+  }
+];
